2389f864516131401729be6fd80dd6473941647e
VladislavShakuroS6 <vladislav.shakuro@sideways6.com>
2021-08-09 18:53:10 +0300

    optimizing code

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -8,16 +8,17 @@ namespace Exercises
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Exercise2();
+            Exercise1();
         }
         static void Exercise1()
         {
+            const double MilimetersInOneInch = 25.4;
             Console.WriteLine("Задача 1:\nЗадано значение длины отрезка в метрах и миллиметрах. Найти ее величину в дюймах.\n");
             Console.WriteLine("Введите значение длины отрезка в метрах:");
             double lenghtMeters = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите значение длины отрезка в миллиметрах:");
             double lenghtMillimeters = Convert.ToDouble(Console.ReadLine());
-            double lenghtInches = (lenghtMeters * 1000 + lenghtMillimeters) / 25.4;
+            double lenghtInches = (lenghtMeters * 1000 + lenghtMillimeters) / MilimetersInOneInch;
             Console.WriteLine($"Значение в дюймах = {lenghtInches}");
         }
 
