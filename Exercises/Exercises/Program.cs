5f5dc1f78b2abf373b04f3cd1659829ad8c0a6e3
VladislavShakuroS6 <vladislav.shakuro@sideways6.com>
2021-08-09 18:18:30 +0300

    Exercise 2

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -8,11 +8,11 @@ namespace Exercises
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Exercise1();
+            Exercise2();
         }
         static void Exercise1()
         {
-            Console.WriteLine("Условие:\nЗадано значение длины отрезка в метрах и миллиметрах. Найти ее величину в дюймах.\n");
+            Console.WriteLine("Задача 1:\nЗадано значение длины отрезка в метрах и миллиметрах. Найти ее величину в дюймах.\n");
             Console.WriteLine("Введите значение длины отрезка в метрах:");
             double lenghtMeters = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите значение длины отрезка в миллиметрах:");
@@ -23,7 +23,15 @@ namespace Exercises
 
         static void Exercise2()
         {
-
+            Console.WriteLine("Задача 2:\nЗадана длительность интервала времени в годах, месяцах и днях. Найти его величину в днях.");
+            Console.WriteLine("Введите значение в годах");
+            int yearsInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение в месяцах");
+            int monthesInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение в днях");
+            int daysInput = Convert.ToInt32(Console.ReadLine());
+            int valueInDays = yearsInput * 365 + monthesInput * 30 + daysInput;
+            Console.WriteLine($"Величина в днях {valueInDays}");
         }
     }
 }
